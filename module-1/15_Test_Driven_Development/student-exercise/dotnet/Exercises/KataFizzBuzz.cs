def67f072ca695378c5f8417f2ab32b37caa0576
Leslie <leslies.email3@gmail.com>
2020-10-02 21:35:34 -0400

    Changed code to check for numbers between 1 and 100 for DoesFizzBuzzReturnCorrectString

--- module-1/15_Test_Driven_Development/student-exercise/dotnet/Exercises/KataFizzBuzz.cs
+++ module-1/15_Test_Driven_Development/student-exercise/dotnet/Exercises/KataFizzBuzz.cs
@@ -10,20 +10,23 @@ namespace Exercises
     {
         public string FizzBuzz(int input)
         {
-            if (input % 3 == 0 && input % 5 == 0)
+            if (input >= 1 && input <= 100)
             {
-                return "FizzBuzz";
+                if (input % 3 == 0 && input % 5 == 0)
+                {
+                    return "FizzBuzz";
+                }
+                if (input % 3 == 0)
+                {
+                    return "Fizz";
+                }
+                else if (input % 5 == 0)
+                {
+                    return "Buzz";
+                }
+                return input.ToString();
             }
-            if (input % 3 == 0)
-            {
-                return "Fizz";
-            }
-            else if (input % 5 == 0)
-            {
-                return "Buzz";
-            }
-            return input.ToString();
-            
+            return " ";
         }
     }
 }
