ae60cef5ea8de6df80d300026d84f77ca52d9784
Leslie <leslies.email3@gmail.com>
2020-10-05 19:48:01 -0400

    completed file io part1 homework

--- module-1/16_FileIO_Reading_in/student-exercise/dotnet/WordSearch/WordSearchApp.cs
+++ module-1/16_FileIO_Reading_in/student-exercise/dotnet/WordSearch/WordSearchApp.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+using System.Linq;
 
 namespace WordSearch
 {
@@ -13,6 +15,9 @@ namespace WordSearch
             //1. Ask the user for the search string
             Console.WriteLine("Please input what you would like to search for");
             string searchPhrase = Console.ReadLine();
+            Console.WriteLine("Should the search be case sensitive? Y/N");
+            string caseSense = Console.ReadLine();
+
 
             //2. Ask the user for the file path
             Console.WriteLine();
@@ -22,15 +27,29 @@ namespace WordSearch
             //3. Open the file
             using (StreamReader reader = new StreamReader(filePath))
             {
+                int lineCounter = 0;
+                
                 //4. Loop through each line in the file
                 while (!reader.EndOfStream)
                 {
-                    string line = reader.ReadLine();
+                    string line;
+                    if (caseSense == "n" || caseSense == "N" || caseSense == "no" || caseSense == "No")
+                    {
+                        line = reader.ReadLine().ToLower();
+                        //searchPhrase.toLower();  //trying to figure out how to specify case sensitivity.
+                    }
+                    else
+                    {
+                        line = reader.ReadLine();
+                    }
+
                     //5. If the line contains the search string, print it out along with its line number
                     if (line.Contains(searchPhrase))
                     {
-                        Console.WriteLine(line);
+                        Console.WriteLine(lineCounter.ToString() + line);
+
                     }
+                    lineCounter++;
                 }
                 
             }
