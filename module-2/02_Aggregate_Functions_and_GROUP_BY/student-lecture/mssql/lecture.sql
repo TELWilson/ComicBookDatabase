245d26bc83b62029cd89550ea87fb6807d67e390
Leslie <leslies.email3@gmail.com>
2020-10-13 15:11:56 -0400

    lecture udpates and aggregate function homework complete

--- module-2/02_Aggregate_Functions_and_GROUP_BY/student-lecture/mssql/lecture.sql
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/student-lecture/mssql/lecture.sql
@@ -1,38 +1,103 @@
 -- ORDERING RESULTS
 
 -- Populations of all countries in descending order
+SELECT name, population
+FROM country
+ORDER BY population DESC;
 
 --Names of countries and continents in ascending order
+SELECT name, continent
+FROM country
+ORDER BY continent, name ASC
 
 -- LIMITING RESULTS
 -- The name and average life expectancy of the countries with the 10 highest life expectancies.
+
+SELECT TOP 10  name, lifeexpectancy
+FROM country
+ORDER BY lifeexpectancy DESC
+
 
 -- CONCATENATING OUTPUTS
 
 -- The name & state of all cities in California, Oregon, or Washington.
 -- "city, state", sorted by state then city
+SELECT *
+FROM city
+
+SELECT *
+FROM city
+WHERE district IN ('California', 'Oregon', 'Washington')
+
+SELECT name + ', ' + district AS stringName, name, district,*
+FROM city
+WHERE district IN ('California', 'Oregon', 'Washington')
+
+SELECT name + ', ' + district AS stringName
+FROM city
+WHERE district IN ('California', 'Oregon', 'Washington')
+ORDER BY district, name
+
 
 -- AGGREGATE FUNCTIONS
 -- Average Life Expectancy in the World
+SELECT AVG(lifeexpectancy) AS 'World Life Expectancy'
+FROM country
 
 -- Total population in Ohio
+SELECT SUM(population) AS Population
+FROM city
+WHERE district = 'Ohio'
+
 
 -- The surface area of the smallest country in the world
+SELECT TOP(1) name, surfacearea
+FROM country
+ORDER BY surfacearea ASC
 
 -- The 10 largest countries in the world
+SELECT TOP(10) name, surfacearea
+FROM country
+ORDER BY surfacearea DESC
 
 -- The number of countries who declared independence in 1991
+SELECT COUNT(name) AS '1991'
+FROM country
+WHERE indepyear = 1991
 
 -- GROUP BY
 -- Count the number of countries where each language is spoken, ordered from most countries to least
+SELECT language, COUNT(countrycode) AS CountryCount
+FROM countrylanguage
+GROUP BY language
+ORDER BY CountryCount DESC
 
 -- Average life expectancy of each continent ordered from highest to lowest
+SELECT continent, AVG(lifeexpectancy) AS LifeExp
+FROM country
+GROUP BY continent
+ORDER BY LifeExp DESC
 
 -- Exclude Antarctica from consideration for average life expectancy
+SELECT continent, AVG(lifeexpectancy) AS LifeExp
+FROM country
+WHERE lifeexpectancy IS NOT NULL
+GROUP BY continent
+ORDER BY LifeExp DESC
 
 -- Sum of the population of cities in each state in the USA ordered by state name
+SELECT district, SUM(population) AS Pop
+FROM city
+WHERE countrycode = 'USA'
+GROUP BY district
+ORDER BY district
 
 -- The average population of cities in each state in the USA ordered by state name
+SELECT district, AVG(population) AS Pop
+FROM city
+WHERE countrycode = 'USA'
+GROUP BY district
+ORDER BY district
 
 -- SUBQUERIES
 -- Find the names of cities under a given government leader
