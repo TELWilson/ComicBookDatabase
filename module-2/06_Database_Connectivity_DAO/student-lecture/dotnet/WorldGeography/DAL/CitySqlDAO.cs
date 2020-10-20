1ef56ef4b67b2cff8ce2ed6945ba839a8fc28485
Leslie <leslies.email3@gmail.com>
2020-10-20 08:38:20 -0400

    updates

--- module-2/06_Database_Connectivity_DAO/student-lecture/dotnet/WorldGeography/DAL/CitySqlDAO.cs
+++ module-2/06_Database_Connectivity_DAO/student-lecture/dotnet/WorldGeography/DAL/CitySqlDAO.cs
@@ -9,6 +9,8 @@ namespace WorldGeography.DAL
     {
         private string connectionString;
 
+        private string sqlGetCitiesByCountryCode = "SELECT * FROM city WHERE countrycode = @countrycode;";
+
         /// <summary>
         /// Creates a new sql-based city dao.
         /// </summary>
@@ -25,8 +27,41 @@ namespace WorldGeography.DAL
 
         public IList<City> GetCitiesByCountryCode(string countryCode)
         {
-            throw new NotImplementedException();
+            IList<City> cities = new List<City>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(sqlGetCitiesByCountryCode, conn);
+
+                command.Parameters.AddWithValue("@countrycode", countryCode);
+                
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    City city = new City();
+                   
+                    city.CityId = Convert.ToInt32(reader["id"]);
+                    city.Name = Convert.ToString(reader["name"]);
+                    city.CountryCode = Convert.ToString(reader["countrycode"]);
+                    city.District = Convert.ToString(reader["district"]);
+                    city.Population = Convert.ToInt32(reader["population"]);
+
+
+                    cities.Add(city);
+                }
+            }
+
+
+
+
+
+            return cities;
         }
+    }
 
     }
-}
+
--- module-2/06_Database_Connectivity_DAO/student-lecture/dotnet/WorldGeography/DAL/CountrySqlDAO.cs
+++ module-2/06_Database_Connectivity_DAO/student-lecture/dotnet/WorldGeography/DAL/CountrySqlDAO.cs
@@ -9,6 +9,8 @@ namespace WorldGeography.DAL
     {
         private string connectionString;
 
+        private string sqlGetCountry = "SELECT * FROM country;";
+
         /// <summary>
         /// Creates a sql based country dao.
         /// </summary>
@@ -20,7 +22,35 @@ namespace WorldGeography.DAL
 
         public IList<Country> GetCountries()
         {
-            throw new NotImplementedException();
+            IList<Country> countries  = new List<Country>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(sqlGetCountry, conn);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    Country country = new Country();
+                    country.Code = Convert.ToString(reader["code"]);
+                    country.Name = Convert.ToString(reader["name"]);
+                    country.Continent = Convert.ToString(reader["continent"]);
+                    country.Region = Convert.ToString(reader["region"]);
+                    country.SurfaceArea = Convert.ToDouble(reader["surfacearea"]);
+
+            
+                    countries.Add(country);
+                }
+            }
+
+
+
+
+
+            return countries;
         }
 
         public IList<Country> GetCountries(string continent)
