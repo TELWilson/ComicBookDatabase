c940df5d9a2e0ad4fe7509bc3644bc36f46f6fc8
Leslie <leslies.email3@gmail.com>
2020-11-17 17:04:01 -0500

    lecture and homework

--- module-4/07_DOM/student-exercise/shopping-list/js/shopping-list.js
+++ module-4/07_DOM/student-exercise/shopping-list/js/shopping-list.js
@@ -1,23 +1,71 @@
 // add pageTitle
-
+ const pageTitle = 'My Shopping List'
 // add groceries
+const groceries = [
+  {item: 'cheese'},
+  {item: 'popcorn'}, 
+  {item:'toilet paper'}, 
+  {item: 'eggs'}, 
+  {item: 'bread'}, 
+  {item:'mint chip ice cream'}, 
+  {item:'oreos'}, 
+  {item: 'milk'}, 
+  {item: 'kool-aid'}, 
+  {item: 'bananas'}
+]
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+  const title = document.querySelector('#title');
+
+  title.innerText = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+  const groc = document.querySelector('#groceries');
+//forEach over groceries
+groceries.forEach(grocery =>
+  {
+    //Create a new li and store it in a variable named container
+    let container = document.createElement('li');
+
+    //Add the 'grocery' class to the element we just created
+    container.classList.add('grocery');
+
+    container.innerText = grocery.item;
+   
+
+    //addGrocery(container, grocery.groceries);
+     
+
+// Add the new li to the main application area
+    groc.appendChild(container);
+  });
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+function markCompleted() {
+  //querySelectorAll('li')
+  const comp = document.querySelectorAll('li');
+  comp.forEach(li => li.classList.add('completed'));
+  
+
+  //let button = document.querySelector('.btn');
+
+  
+
+  
+
+}
 
 setPageTitle();
 
